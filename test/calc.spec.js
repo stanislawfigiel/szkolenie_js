fa8a47a726596257a1783bebc7af0eb9467d748a
StanislawFigiel <stanislaw.figiel@atos.net>
2019-08-20 14:19:33 +0200

    travis after success

--- test/calc.spec.js
+++ test/calc.spec.js
@@ -4,15 +4,9 @@ const assert = require('assert');
 
 describe('calcul', () => {
 
-    it('should add numbers',() =>
-{
-    assert.equal(Calculator.add(2, 2), 4)
-    // assert.equal(Calculator.add(2, 2), 6)
+    it('should add numbers', () => {
+            assert.equal(Calculator.add(2, 2), 4)
+        }
+    )
 
-
-
-
-}
-)
-
-}  )
+})
