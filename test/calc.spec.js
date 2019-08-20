71e75d867a01159763a01258c030527c2aaf05d6
StanislawFigiel <stanislaw.figiel@atos.net>
2019-08-20 13:39:13 +0200

    init commit

---
+++ test/calc.spec.js
@@ -0,0 +1,18 @@
+const {Calculator} = require('../src/calc');
+
+const assert = require('assert');
+
+describe('calcul', () => {
+
+    it('should add numbers',() =>
+{
+    assert.equal(Calculator.add(2, 2), 4)
+    // assert.equal(Calculator.add(2, 2), 6)
+
+
+
+
+}
+)
+
+}  )
